1dc8909c8a98aa2cc3c1f1544aaa8ee8e321cdbe
N3ROO <swellchaser33@gmail.com>
2020-09-20 19:39:22 +0200

    working 1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![logo](https://github.com/N3ROO/HorseStatsMod/raw/MC_1.16.3/.github/resources/quicksearch.png)
+![logo](https://github.com/N3ROO/HorseStatsMod/raw/MC_1.16.3/.github/resources/horsestatsmod.png)
 
 # Horse Stats Mod
 ![github downloads](https://img.shields.io/github/downloads/n3roo/horsestatsmod/total.svg?label=github%20downloads)
@@ -7,9 +7,9 @@
 ![release](https://img.shields.io/github/release/n3roo/horsestatsmod.svg)
 ![build](https://img.shields.io/github/workflow/status/N3ROO/horsestatsmod/Build%20MC1.16.3?label=build%201.16.3)
 
-A mod that shows the hidden horses' stats (jump height and speed). 
+A lightweight mod that shows the horses' stats (health, jump height and speed). 
 
-![demo](https://github.com/N3ROO/HorseStatsMod/raw/MC_1.16.3/.github/resources/demo-1.0.0.gif)
+![demo](https://github.com/N3ROO/HorseStatsMod/raw/MC_1.16.3/.github/resources/demo-1.0.0.png)
 
 ## Contributing
 Don't hesitate to contribute. Even if it's your first time, it's never too late to learn!
--- src/main/java/dev/nero/horsestatsmod/HorseStatsMod.java
+++ src/main/java/dev/nero/horsestatsmod/HorseStatsMod.java
@@ -11,11 +11,24 @@ package dev.nero.horsestatsmod;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.client.gui.screen.inventory.HorseInventoryScreen;
+import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.passive.horse.AbstractHorseEntity;
+import net.minecraft.entity.passive.horse.HorseEntity;
+import net.minecraft.util.text.*;
+import net.minecraft.world.BossInfo;
 import net.minecraftforge.client.event.GuiContainerEvent;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.gui.GuiUtils;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod("horsestatsmod")
 public class HorseStatsMod
@@ -28,24 +41,121 @@ public class HorseStatsMod
     }
 
     private void onDrawForegroundEvent(final GuiContainerEvent.DrawForeground event) {
-        // It draws the search box and it also highlights the found slots
-        /*
-        if (this.isSearchableContainer(event.getGuiContainer())) {
-            // Render search text
-            RenderUtils.renderText(
-                    this.isSearching ?
-                            "Search: " + searchText + "_" :
-                            "Search disabled (" + keybindToString(this.focusKeybinding) + ")",
-                    0,
-                    - Minecraft.getInstance().fontRenderer.FONT_HEIGHT - 5,
-                    this.isSearching ? 0xFFFFFF : 0x888888
-            );
+        if (event.getGuiContainer() instanceof HorseInventoryScreen) {
+            try {
+                AbstractHorseEntity horse = (AbstractHorseEntity) Minecraft.getInstance().player.getRidingEntity();
 
-            // Highlight slots
-            for (SlotPos slotPos : this.highlightedSlots) {
-                RenderUtils.highlightSlot(slotPos, 0x772fb000); // green with transparency
+                double health = horse.getAttribute(Attributes.field_233818_a_).getValue();
+                double jumpHeight = horse.getAttribute(Attributes.field_233830_m_).getValue();
+                double speed = horse.getAttribute(Attributes.field_233821_d_).getValue();
+
+                jumpHeight = (
+                        - 0.1817584952 * Math.pow(jumpHeight, 3) +
+                        3.689713992 * Math.pow(jumpHeight, 2) +
+                        2.128599134 * jumpHeight - 0.343930367
+                ); // convert to blocks
+                speed = speed * 43; // convert to m/s
+
+                double mouseX = Minecraft.getInstance().mouseHelper.getMouseX() - 2*event.getGuiContainer().getGuiLeft();
+                double mouseY = Minecraft.getInstance().mouseHelper.getMouseY() - 2*event.getGuiContainer().getGuiTop();
+
+                double rw = 30;
+                double rh = 11;
+                double ry = 6;
+                double rx;
+
+                // Stats
+                rx = 60;
+                this.renderText("Stats:", (int) rx, (int) ry, 0X444444);
+
+                // Health
+                rx += 30;
+                this.renderText(
+                        String.format("%.2f", health),
+                        (int) rx, (int) ry,
+                        getColor(health, 15, 30)
+                );
+                if (posInRect(mouseX, mouseY, (rx-2)*2,  (ry-2)*2, rw*2, rh*2)) {
+                    drawHoveringText(
+                            (int) mouseX/2, (int) mouseY/2,
+                            "Health:", "15.0", "30.0", "player: 20.0"
+                    );
+                }
+
+                // Jump
+                rx += 30;
+                this.renderText(
+                        String.format("%.2f", jumpHeight),
+                        (int) rx, (int) ry,
+                        getColor(jumpHeight, 1.25, 5)
+                );
+                if (posInRect(mouseX, mouseY, (rx-2)*2,  (ry-2)*2, (rw-6)*2, rh*2)) {
+                    drawHoveringText(
+                            (int) mouseX/2, (int) mouseY/2,
+                            "Jump (blocks):", "1.25", "5.0", "player: 1.25"
+                    );
+                }
+
+                // Speed
+                rx += 24;
+                this.renderText(
+                        String.format("%.2f", speed),
+                        (int) rx, (int) ry,
+                        getColor(speed, 4.8, 14.5)
+                );
+                if (posInRect(mouseX, mouseY, (rx-2)*2,  (ry-2)*2, rw*2, rh*2)) {
+                    drawHoveringText(
+                            (int) mouseX/2, (int) mouseY/2,
+                            "Speed (m/s):", "4.8", "14.5",
+                            "player: 4.317 (walk)", "player: 5.612 (sprint)", "player: 7.143 (sprint+jump)"
+                    );
+                }
+            } catch (Exception e) {
+                LOGGER.error("The player is looking into an horse inventory without riding it? Is that possible?");
             }
-        }*/
+        }
+    }
+
+    private void drawHoveringText(int x, int y, String title, String min, String max, String... notes) {
+        List<ITextProperties> textLines = new ArrayList<>();
+        textLines.add(new StringTextComponent(title));
+        textLines.add(new StringTextComponent(TextFormatting.RED + "min: " + min));
+        textLines.add(new StringTextComponent(TextFormatting.GREEN + "max: " + max));
+        for (String note : notes) {
+            textLines.add(new StringTextComponent(note));
+        }
+
+        GuiUtils.drawHoveringText(
+                new MatrixStack(), textLines,
+                x,
+                y,
+                Minecraft.getInstance().getMainWindow().getWidth(),
+                Minecraft.getInstance().getMainWindow().getHeight(),
+                150, Minecraft.getInstance().fontRenderer
+        );
+    }
+
+    /**
+     * @param val the value
+     * @param min the min possible value
+     * @param max the max possible value
+     * @return an hex color according to the percentage of val in min/max
+     */
+    private int getColor(double val, double min, double max) {
+        val = val - min;
+        max = max - min;
+
+        double p = 100d * val / max;
+
+        if (p <= 25) {
+            return 0xd12300;
+        } else if (p > 25 && p <= 50) {
+            return 0xd18800;
+        } else if (p > 50 && p <= 75) {
+            return 0xfae314;
+        } else {
+            return 0x77bf04;
+        }
     }
 
     /**
@@ -56,13 +166,26 @@ public class HorseStatsMod
      * @param y the y position (from top to bottom),
      * @param color the color in hex (00-FF), following this format: RRGGBB (R:red, G:green, B:blue). Ex: 0xFFFFFF
      */
-    public static void renderText(String text, int x, int y, int color) {
-        Minecraft.getInstance().fontRenderer.func_238405_a_(
+    private void renderText(String text, int x, int y, int color) {
+        Minecraft.getInstance().fontRenderer.func_238421_b_(
                 new MatrixStack(),
                 text,
                 x,
                 y,
                 color
         );
+    }
+
+    /**
+     * @param px x position to test
+     * @param py y position to test
+     * @param rx x position of rect
+     * @param ry y position of rect
+     * @param rw width of rect
+     * @param rh height of rect
+     * @return true if the given position is in the given rectangle
+     */
+    private boolean posInRect(double px, double py, double rx, double ry, double rw, double rh) {
+        return (px >= rx && px <= rx + rw) && (py >= ry && py <= ry + rh);
     }
 }
