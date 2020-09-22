7fecf2f2f9c77dcb2762ad9c53a356715732c741
N3ROO <swellchaser33@gmail.com>
2020-09-22 18:03:57 +0200

    added config

---
+++ src/main/java/dev/nero/horsestatsmod/config/ClientConfig.java
@@ -0,0 +1,27 @@
+package dev.nero.horsestatsmod.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class ClientConfig {
+    public final ForgeConfigSpec.BooleanValue displayStats;
+    public final ForgeConfigSpec.BooleanValue coloredStats;
+
+    public ClientConfig(ForgeConfigSpec.Builder builder) {
+        // builder.push("category");
+
+        displayStats = builder
+                .comment("Shows the stats in the GUI. If turned off, you need to put your mouse on the horse name " +
+                         "(in the GUI) to show a tooltip with its stats. Can be useful with some resource packs or " +
+                         "mods that change the GUI of horses")
+                .translation("horsestatsmod" + ".config." + "displayStats")
+                .define("displayStats", true);
+
+        coloredStats = builder
+                .comment("Shows the stats with colors (ignored if displayStats is false). If turned on, it will " +
+                         " display the stats with the default gray color. Can be useful with some resource packs.")
+                .translation("horsestatsmod" + ".config." + "coloredStats")
+                .define("coloredStats", false);
+
+        builder.pop();
+    }
+}
---
+++ src/main/java/dev/nero/horsestatsmod/config/TheModConfig.java
@@ -0,0 +1,50 @@
+package dev.nero.horsestatsmod.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import org.apache.commons.lang3.tuple.Pair;
+
+@Mod.EventBusSubscriber(modid = "horsestatsmod", bus = Mod.EventBusSubscriber.Bus.MOD)
+public class TheModConfig {
+
+    public static final ClientConfig CLIENT;
+    public static final ForgeConfigSpec CLIENT_SPEC;
+    static {
+        final Pair<ClientConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ClientConfig::new);
+        CLIENT_SPEC = specPair.getRight();
+        CLIENT = specPair.getLeft();
+    }
+
+    private static boolean displayStats;
+    private static boolean coloredStats;
+
+    @SubscribeEvent
+    public static void onModConfigEvent(final ModConfig.ModConfigEvent configEvent) {
+        if (configEvent.getConfig().getSpec() == TheModConfig.CLIENT_SPEC) {
+            TheModConfig.bakeConfig();
+        }
+    }
+
+    public static void bakeConfig() {
+        displayStats = CLIENT.displayStats.get();
+        coloredStats = CLIENT.coloredStats.get();
+    }
+
+    public static boolean isDisplayStats() {
+        return displayStats;
+    }
+
+    public static void setDisplayStats(boolean displayStats) {
+        TheModConfig.displayStats = displayStats;
+    }
+
+    public static boolean isColoredStats() {
+        return coloredStats;
+    }
+
+    public static void setColoredStats(boolean coloredStats) {
+        TheModConfig.coloredStats = coloredStats;
+    }
+}
